e2d4d5affd6c47c7c140218ab9b82f83c0b836db
Vlad <gerdoexx@gmail.com>
2020-08-19 21:28:05 +0300

    clean up

--- src/game/server/gamecontext.h
+++ src/game/server/gamecontext.h
@@ -121,7 +121,7 @@ public:
 
 	// helper functions
 	void CreateDamageInd(vec2 Pos, float AngleMod, int Amount, int64_t Mask=-1);
-	void CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamage, int ActivatedTeam, int64_t Mask);
+	void CreateExplosion(vec2 Pos, int Owner, int Weapon, bool NoDamage, int ActivatedTeam, int64_t Mask = -1);
 	void DetonateSlowBomb(vec2 Pos);
 	void CreateHammerHit(vec2 Pos, int64_t Mask=-1);
 	void CreatePlayerSpawn(vec2 Pos, int64_t Mask=-1);
